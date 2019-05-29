0a94de4a68c357c324f536433684d97432b5197b
yiz <ql3895619@gmail.com>
2019-05-29 11:38:16 +0800

    feat: convert files in mp3/

--- convert.py
+++ convert.py
@@ -1,6 +1,15 @@
 from pydub import AudioSegment
-sound = AudioSegment.from_mp3("./mp3/Taiwanese/2.mp3")
-sound =sound.set_frame_rate(8192)
-sound = sound.set_channels(1)
-sound.export("./wav/Taiwanese/2.wav", format="wav")
-print(sound.frame_rate)
+from glob import glob
+
+def convert2wav(filename):
+    sound = AudioSegment.from_mp3(filename)
+    sound =sound.set_frame_rate(8192)
+    sound = sound.set_channels(1)
+
+    wavfilename = filename.replace('mp3', 'wav')
+    sound.export(wavfilename, format="wav")
+
+if "__main__" == __name__:
+    filenames = glob(r'./mp3/**/*.mp3', recursive=True)
+    for filename in filenames:
+        convert2wav(filename)
