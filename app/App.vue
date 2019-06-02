47ea1a2e844c20d8ead108ee1a63fdef96bd636a
yiz <ql3895619@gmail.com>
2019-06-03 01:06:52 +0800

    feat: basic ui of micorphone

--- app/App.vue
+++ app/App.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
 #app
+  .ui.active.dimmer
+    i.ui.icon.hourglass.half.big#time(v-if="'timing'==status") :{{ countdown }}
+    .ui.loader.massive(v-if="'analizing'==status")
+    .circular.ui.icon.button(@click="start_record")
+      i.microphone.icon.huge
+
+
 </template>
 
 <script>
@@ -8,9 +15,44 @@ const axios = require('axios')
 export default {
 
   data() { return {
+    status: '',
+    countdown: 10
   }},
 
   methods: {
+    start_record () {
+      this.status = 'timing'
+      setInterval(() => this.countdown--, 1000)
+      setTimeout(() => this.stop_record(), 10000)
+
+      navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then(this.handleSuccess)
+    },
+    handleSuccess (stream) {
+      const options = {mimeType: 'audio/webm'}
+      const recordedChunks = []
+      const mediaRecorder = new MediaRecorder(stream, options)
+
+      mediaRecorder.addEventListener('dataavailable', function(e) {
+	if (e.data.size > 0) {
+	  recordedChunks.push(e.data)
+	}
+      })
+
+      mediaRecorder.addEventListener('stop', function() {
+        console.log("analizing...")
+        // axios.post(`/upload`, recordedChunks)
+	// downloadLink.href = URL.createObjectURL(new Blob(recordedChunks))
+	// downloadLink.download = 'acetest.wav'
+      })
+
+      mediaRecorder.start()
+      setTimeout(() => mediaRecorder.stop(), 10000)
+    },
+    stop_record () {
+      this.status = 'analizing'
+    }
+
       // axios.post(`/upload`, speech)
   },
 
@@ -19,8 +61,12 @@ export default {
 
 <style lang="sass" scoped>
 #app
-  background-color: rgba(0,0,0,.05)
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif
+.button
+  position: fixed
+  bottom: 10vh
+#time
+  color: white
 </style>`
 
 <!--
